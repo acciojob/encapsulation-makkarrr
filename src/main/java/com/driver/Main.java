e3298f146bc45afc38ba1a48ac12db16efce8d12
akankshamakkar <akimakkar1282@gmai.com>
2023-05-11 16:12:15 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void Main() {
+        RWOnly rwOnly = new RWOnly();
+        rwOnly.setName("Akanksha");
+        String name = rwOnly.getName();
+    }
+
+
 }
